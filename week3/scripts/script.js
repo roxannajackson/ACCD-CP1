d133d9aeadb268358d7374cb28b34746c9e24002
roxannajackson <roxannarjackson@gmail.com>
2025-10-04 20:47:52 -0700

    Update the code to match what was made in class

--- week3/scripts/script.js
+++ week3/scripts/script.js
@@ -1,1 +1,30 @@
-console.log("Hello, World!");
+let cStage = document.getElementById("colorStage")
+let cButton = document.getElementById("colorButton")
+
+
+const qImage = document.getElementById("quokkaImage")
+const qButton = document.getElementById("imageToggle")
+
+let changeColor = function() 
+{
+    let rComp = Math.random() * 255
+    let gComp = Math.random() * 255
+    let bComp = Math.random() * 255
+
+    cStage.style.backgroundColor = "rgb(" + rComp + ", " + gComp + ", " + bComp + ")"
+}
+
+let toggleImage = () =>
+{
+    console.log(qImage.src)
+    if(qImage.src.includes("quokka1")) {
+        qImage.src = "images/quokka2.jpg"
+    }
+    else {
+        qImage.src = "images/quokka1.jpg"
+    }
+}
+
+qButton.addEventListener("click", toggleImage)
+cButton.addEventListener("click", changeColor);
+window.addEventListener("load", changeColor)
