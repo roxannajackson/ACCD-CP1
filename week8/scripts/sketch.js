8c850348b9de0987a9a0a9684ee96e8b531205bf
roxannajackson <roxannarjackson@gmail.com>
2025-11-18 15:18:14 -0800

    Added the details and my process
    I added my process and details for achieving the calm, pulsing particles to look like an orbit

--- week8/scripts/sketch.js
+++ week8/scripts/sketch.js
@@ -1,19 +1,24 @@
+let cnv = createCanvas(windowWidth, windowHeight);
+cnv.position(0, 0);
+cnv.style('z-index', '-1');
 
 let particles = [];
-let numParticles = 300;
-let circleRadius = 200;
+let imgSpacedock;
+
+function preload() {
+  imgSpacedock = loadImage("spacedocklogo.png");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   colorMode(HSB, 360, 100, 100);
   noStroke();
 
-  for (let i = 0; i < numParticles; i++) {
+  for (let i = 0; i < 300; i++) {
     let angle = random(TWO_PI);
-    let r = circleRadius + random(-20, 20);
     particles.push({
-      angle: angle,
-      r: r,
+      angle,
+      r: 200 + random(-20, 20),
       hue: random(20, 50),
       size: random(3, 8)
     });
@@ -21,16 +26,23 @@ function setup() {
 }
 
 function draw() {
-  background(0, 0, 10, 0.2); // subtle trail effect
+  background(0, 0, 5);
   translate(width / 2, height / 2);
 
-  for (let p of particles) {
-    let pulse = sin(frameCount * 0.05 + p.angle * 5) * 10;
-    let x = (p.r + pulse) * cos(p.angle);
-    let y = (p.r + pulse) * sin(p.angle);
+  // Floating Image (Spacedock Logo)
+imageMode(CENTER);
+let w = 200; 
+let h = w * (imgSpacedock.height / imgSpacedock.width); 
+image(imgSpacedock, 0, sin(frameCount * 0.03) * 8, w, h);;
 
+  // Rotation
+  rotate(frameCount * 0.002);
+  
+  // Particles
+  for (let p of particles) {
+    let pulse = sin(frameCount * 0.03 + p.angle * 5) * 5;
+    ellipse((p.r + pulse) * cos(p.angle), (p.r + pulse) * sin(p.angle), p.size);
     fill((p.hue + frameCount) % 360, 80, 100);
-    ellipse(x, y, p.size);
   }
 }
 
